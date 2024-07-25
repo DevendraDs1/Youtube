4d74fb003caa7cc99cbf2dda162b3f162c194a55
DevendraDs1 <113992037+DevendraDs1@users.noreply.github.com>
2024-07-25 18:25:15 +0000

    conversion of datatypes

---
+++ 01_basics/03_convertOperation.js
@@ -0,0 +1,27 @@
+let score="33abc";
+// console.log(score);
+
+let valueinumber = Number(score)
+// console.log(typeof valueinumber);
+// console.log(valueinumber);
+
+
+//"33" => 33
+// "33abc"=>Nan (not a number)
+// true =>1 false => o
+
+let isString="dev"
+
+let booleanisstring=Boolean(isString)
+console.log(booleanisstring);
+
+//1 = true 0 =>false
+//"" =>false 
+//"dev"=>true
+
+let somenum=33
+
+let stringnumber =String(somenum)
+
+console.log(stringnumber);
+console.log(typeof stringnumber);
