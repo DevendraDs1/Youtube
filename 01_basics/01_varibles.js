f9946c432912562e05c3d188ab629fce3529698f
DevendraDs1 <113992037+DevendraDs1@users.noreply.github.com>
2024-07-26 18:48:51 +0000

    datatypes

---
+++ 01_basics/01_varibles.js
@@ -0,0 +1,10 @@
+const accountId="12345"
+let accountusername="ds"
+var account password="123321"
+accountcity="burhanpur"
+/*
+prefer not to use var because of issue in block dcope and in functional scope and 
+*/
+
+console.table([accountId,accountusername,accountcity,acc])
+
--- 01_basics/03_convertOperation.js
+++ 01_basics/03_convertOperation.js
@@ -1,27 +1,32 @@
-let score="33abc";
-// console.log(score);
+// let score="33abc";
+// // console.log(score);
 
-let valueinumber = Number(score)
-// console.log(typeof valueinumber);
-// console.log(valueinumber);
+// let valueinumber = Number(score)
+// // console.log(typeof valueinumber);
+// // console.log(valueinumber);
 
 
-//"33" => 33
-// "33abc"=>Nan (not a number)
-// true =>1 false => o
+// //"33" => 33
+// // "33abc"=>Nan (not a number)
+// // true =>1 false => o
 
-let isString="dev"
+// let isString="dev"
 
-let booleanisstring=Boolean(isString)
-console.log(booleanisstring);
+// let booleanisstring=Boolean(isString)
+// console.log(booleanisstring);
 
-//1 = true 0 =>false
-//"" =>false 
-//"dev"=>true
+// //1 = true 0 =>false
+// //"" =>false 
+// //"dev"=>true
 
-let somenum=33
+// let somenum=33
 
-let stringnumber =String(somenum)
+// let stringnumber =String(somenum)
 
-console.log(stringnumber);
-console.log(typeof stringnumber);
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
+
+
+// ************ Operation ************* //
+
+
